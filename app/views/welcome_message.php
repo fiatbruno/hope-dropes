dda90d9fe8653421580ea213e743e624c60c6488
fiat_bruno <fiatbruno10@gmail.com>
2021-06-16 12:11:40 +0200

    Added Welcome Page

--- app/views/welcome_message.php
+++ app/views/welcome_message.php
@@ -1,92 +1,131 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	span{
-		font-size: larger;
-	}
-	</style>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Hope dropes</title>
 </head>
+<style>
+	body{
+		font-family: "Poppins";
+	}
+	.home-container {
+		background-image: radial-gradient(white, silver);
+		width: 100.81%;
+	}
+
+	.btn {
+		padding: 5px 10px;
+	}
+
+	.content {
+		margin: 150px 100px;
+	}
+
+	nav {
+		z-index: 999;
+	}
+</style>
+
 <body>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter! By <span>fiat_bruno</span></h1>
+	<div data-bs-spy="scroll" data-bs-target="#navlinks" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+		<nav class="navbar navbar-expand-lg navbar-light col-ms-12 col-md-12 bg-body position-sticky top-0 shadow-sm p-3 rounded" id="navlinks">
+			<div class="container-fluid">
+				<a class="navbar-brand text-success" style="text-shadow: 0px 1px black; font-weight: bolder" href="#"><img src="<?php echo base_url() ?>assets/images/blood.jpg" style="width: 50px;"></a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+					<div class="navbar-nav">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+						<a class="nav-link" href="#about">About Us</a>
+						<a class="nav-link" href="#services">Services</a>
+						<a class="nav-link" href="#table">Help</a>
+					</div>
+				</div>
+				<div>
+					<a href="<?php echo base_url() ?>auth/login" class="btn btn-danger shadow-sm" style="padding: 5px 10px">Sign in</a>
+				</div>
+			</div>
+		</nav>
+		<div class="home-container row" id="home">
+			<div class="col container justify-content-center p-4" style="margin-left: 40px;">
+				<div class="col-8 content">
+					<h1>Donate blood save lives!</h1>
+					<p class=" text-secondary">Saving people's lives is something which you can provide easly. What you need to do is first the heart to give.
+						We are here to help those who are willing to donnate blood to those in need.
+					</p>
+					<p><button type="submit" class="btn-danger shadow-sm row" style="padding: 5px 20px; border: none">Donate</button></p>
+				</div>
+			</div>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+			<div class="col container">
+				<img src="<?php echo base_url() ?>assets/images/doctor.png">
+			</div>
+		</div>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<div class="col-md-11 col-ms-11 p-5 m-5 justify-content-center" id="about">
+			<div class="row row-cols-1 row-cols-md-2 g-2 row-cols-ms-5">
+				<div class="col-6">
+					<div class="card h-100 m-1" style="border: none;">
+						<div class="card-body col-10">
+							<h2 class="text-center text-dark mb-3">About Us</h2>
+							<p class="card-text">Each and every person can expore the hospital. To know more about the hospital using the hospital website. As long as you have the heart to donate to the needy people you can use this app to help needy ones.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-6">
+					<div class="card h-100 m-1" style="border: none;">
+						<img src="<?= base_url() ?>assets/images/post-img.jpg" class="card-img-top" alt="img3" style="height: 300px;">
+					</div>
+				</div>
+			</div>
+		</div>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+		<div class="col-md-11 col-ms-11 p-5 m-5 justify-content-center" id="services">
+			<h2 class="text-center text-dark mb-3">Our Services</h2>
+			<div class="row row-cols-1 row-cols-md-3 g-4 row-cols-ms-5">
+				<div class="col">
+					<div class="card h-100 shadow m-1" style="border: none;">
+						<img src="<?= base_url() ?>assets/images/donation.jpg" class="card-img-top" alt="img3" style="height: 200px;">
+						<div class="card-body">
+							<h3 class="card-title">Explore Hospital</h3>
+							<p class="card-text">Each and every person can expore the hospital. To know more about the hospital using the hospital website.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col">
+					<div class="card h-100 shadow m-1" style="border: none;">
+						<img src="<?= base_url() ?>assets/images/post-img.jpg" class="card-img-top" alt="img2" style="height: 200px;">
+						<div class="card-body">
+							<h3 class="card-title">Donate blood</h3>
+							<p class="card-text">A person who is willing to donate blood can donate it to the patient in the hospital. And this is the very amazing action which is really great!</p>
+						</div>
+					</div>
+				</div>
+				<div class="col">
+					<div class="card h-100 shadow m-1" style="border: none;">
+						<img src="<?= base_url() ?>assets/images/mysvg.jpg" class="card-img-top" alt="img1" style="height: 200px;">
+						<div class="card-body">
+							<h3 class="card-title">Doctor appointment</h3>
+							<p class="card-text">Any person can book an appointment with the doctor about when to donate the blood. Then when time has reached a donnor can come to the hospital to donate blood.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<div class="form">
+
+		</div>
+
 	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+</script>
+
 </html>
