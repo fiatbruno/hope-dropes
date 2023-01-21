0a37ad2b7f8e188e624b36fdd53fca3f4de1360d
Fiat Bruno <64164884+fiatbruno@users.noreply.github.com>
2023-01-21 17:27:54 +0200

    Update Email.php

--- app/controllers/Email.php
+++ app/controllers/Email.php
@@ -19,10 +19,10 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'fiatbruno96@gmail.com';                     //SMTP username
-    $mail->Password   = 'GirishyA10@';                               //SMTP password
+    $mail->Username   = 'fiatbr@gmail.com';                     //SMTP username
+    $mail->Password   = 'pass';                               //SMTP password
     $mail->SMTPSecure = "tls";         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = adfasdf;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
 
